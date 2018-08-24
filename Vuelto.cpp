570a7a1af82f58f5f3fb9933ab76e0f03a6401ef
ALVARO CERNA <31244499+Alvarofrk@users.noreply.github.com>
2018-08-24 07:51:48 -0500

    Update Vuelto.cpp

--- Vuelto.cpp
+++ Vuelto.cpp
@@ -5,91 +5,91 @@ using namespace std;
 
 
 void vuelto(double &x){
-	long positivo;	
+	long positivo;
 	double decimal;
 	double aux=0;
 	positivo = long(x);
 	decimal = (((positivo-x)*100)*(-1));
 	
 	//variables
-	int billetes_200 = 0; 
-    int billetes_100 = 0; 
-    int billetes_50 = 0; 
-    int billetes_20 = 0; 
-    int billetes_10 = 0; 
-    int monedas_5 = 0; 
-    int monedas_2 = 0; 
-    int monedas_1 = 0; 
-    int centimos_50 = 0; 
+	int billetes_200 = 0;
+	int billetes_100 = 0;
+	int billetes_50 = 0;
+	int billetes_20 = 0;
+	int billetes_10 = 0;
+	int monedas_5 = 0;
+	int monedas_2 = 0;
+	int monedas_1 = 0;
+	int centimos_50 = 0;
 	int centimos_20 = 0;
 	int centimos_10 = 0;
 	
 	//billetes
-	for (positivo; positivo >= 200 ;) 
-    { 
-        billetes_200 += 1; 
-        positivo -= 200; 
-    } 
-    if ((positivo < 200) && (positivo >= 100)) 
-    { 
-        billetes_100 += 1; 
-        positivo -= 100; 
-    } 
-    if((positivo < 100) && (positivo >= 50)) 
-    { 
-        billetes_50 += 1; 
-        positivo -= 50; 
-    } 
-    for(positivo;(positivo < 50) && (positivo >= 20);) 
-    { 
-        billetes_20 += 1; 
-        positivo -= 20; 
-    } 
-    if((positivo < 20) && (positivo >= 10)) 
-    { 
-        billetes_10 += 1; 
-        positivo -= 10; 
-    } 
-    
-    //monedas
-    if ((positivo < 10) && (positivo >= 5)) 
-    { 
-        monedas_5 += 1; 
-        positivo -= 5; 
-    } 
-    for (positivo;(positivo < 5) && (positivo >= 2);) 
-    { 
-        monedas_2 += 1; 
-        positivo -= 2; 
-    } 
-    if ((positivo < 2) && (positivo >=1)) 
-    { 
-        monedas_1 += 1; 
-        positivo -= 1; 
-    } 
-    
-    //centimos
-    if((decimal < 100 ) && (decimal>49))
-    {
-    	centimos_50 +=1;
-    	decimal -=50;
+	for (positivo; positivo >= 200 ;)
+	{
+		billetes_200 += 1;
+		positivo -= 200;
+	}
+	if ((positivo < 200) && (positivo >= 100))
+	{
+		billetes_100 += 1;
+		positivo -= 100;
+	}
+	if((positivo < 100) && (positivo >= 50))
+	{
+		billetes_50 += 1;
+		positivo -= 50;
+	}
+	for(positivo;(positivo < 50) && (positivo >= 20);)
+	{
+		billetes_20 += 1;
+		positivo -= 20;
+	}
+	if((positivo < 20) && (positivo >= 10))
+	{
+		billetes_10 += 1;
+		positivo -= 10;
+	}
+	
+	//monedas
+	if ((positivo < 10) && (positivo >= 5))
+	{
+		monedas_5 += 1;
+		positivo -= 5;
+	}
+	for (positivo;(positivo < 5) && (positivo >= 2);)
+	{
+		monedas_2 += 1;
+		positivo -= 2;
+	}
+	if ((positivo < 2) && (positivo >=1))
+	{
+		monedas_1 += 1;
+		positivo -= 1;
+	}
+	
+	//centimos
+	if((decimal < 100 ) && (decimal>49))
+	{
+		centimos_50 +=1;
+		decimal -=50;
 	}
 	
 	for(decimal;(decimal < 50) && (decimal>19);)
-    {
-    	centimos_20 +=1;
-    	decimal -=20;
+	{
+		centimos_20 +=1;
+		decimal -=20;
 	}
-
+	
 	
 	if((decimal < 20) && (decimal>9))
-    {
-    	centimos_10 +=1;
-    	decimal -=10;
+	{
+		centimos_10 +=1;
+		decimal -=10;
 	}
 	
-
-
+	
+	
 	//imprimir
 	if(billetes_200!=0){
 		cout<<billetes_200<<" de S./200.00"<<endl;
@@ -131,15 +131,16 @@ void vuelto(double &x){
 	if(centimos_10!=0){
 		cout<<centimos_10<<" de S./0.10"<<endl;
 	}
-
-
-
+	
+	
+	
 }
 
 int main(){
 	
 	double *x;
-	double y=189.20;
+	double y;
+	cout<<"ingresa tu cantidad de dinero: ";cin>>y;
 	x=&y;
 	
 	vuelto(*x);
