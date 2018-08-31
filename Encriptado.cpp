667b5f9bfead10cb2984ec6a5228336f73a683fb
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-08-31 01:40:17 -0500

    Update Encriptado.cpp

--- Encriptado.cpp
+++ Encriptado.cpp
@@ -1,38 +1,79 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int Invmod(int num, int mod) {
-    int q[10];
-    int residuo = mod % num, mod_aux = mod, i = 0;
-    while (residuo >= 1) {
-	   q[i] = (mod_aux - residuo) / num;
-	   mod_aux = num;
-	   num = residuo;
-	   residuo = mod_aux % num;
-	   i++;
+int invm(int a, int b) {
+    int r;
+    int r1 = a, r2 = b;
+    int s1 = 1, s2 = 0;
+    int s;
+    int t1 = 0, t2 = 1;
+    int t, q;
+    while (r2>0) {
+	   q = r1 / r2;
+	   r = r1 - (q*r2);
+	   r1 = r2; r2 = r;
+
+	   s = s1 - (q*s2);
+	   s1 = s2; s2 = s;
+
+	   t = t1 - (q*t2);
+	   t1 = t2; t2 = t;
+    }
+    if (r1 != 1) {
+	   return 0;
+    }
+    else {
+	   int resto = s1 + s2;
+	   if (resto<0) {
+		  resto = b + resto;
+	   }
+	   return resto;
+    }
+}
+
+void Encriptado(string mensaje,int clave,int mens_encrip[]) {
+    int carac = 256;
+    for (int i = 0; i<mensaje.length(); i++) {
+	   cout << mensaje[i] + 0 << endl;
+	   if (invm(mensaje[i], carac) == 0) {
+		  cout << "Palabra invalida,digite otra" << endl;}
+		  //Encriptado();}
+	   mens_encrip[i] = mensaje[i] * clave;
+	   mens_encrip[i] %= 256;
     }
 
-    int p[] = { 0,0,0,0,0,0,0,0,0,0 }; p[0] = 0; p[1] = 1;
-
-    for (int j = 2; j <= i + 2; j++) {
-	   p[j] = p[j - 2] - (p[j - 1] * q[j - 2]);
-	   if (p[j] < 0) {
-		  p[j] = mod + p[j];
-	   }
-	   else if (p[j] > mod) {
-		  p[j] -= mod;
-	   }
+    for (int i = 0; i < mensaje.length(); i++) {
+	   cout <<"encrip:"<< mens_encrip[i] << endl;
     }
+}
 
-    return p[i+1];
+void Desencriptado(string mensaje, int clave, int mens_encrip[]) {
+    int carac = 256; int clave_public = invm(clave, carac);
+    for (int i = 0; i < mensaje.length(); i++) {
+	   mens_encrip[i] *= clave_public;
+	   mens_encrip[i] %= 256;
+    }
+    for (int i = 0; i < mensaje.length(); i++) {
+	   cout << "Desencrip:" << mens_encrip[i] << endl;
+    }
 }
 
 int main() {
-    int cla_public = 0, cla_private = 0,carac=256;
+    int carac = 256;
+    string mensaje;  int mens_encrip[10];
+    int clave_pri = 0;
+    cout << "Dame una clave o te asalto csmre >:v : " << endl;
+    cin >> clave_pri;
+    if (invm(clave_pri, carac) == 0) {
+	   cout << "Clave sin inversa, digite otra" << endl;
+	   main();
+    }
+    cout << "Palabra: " << endl; cin >> mensaje;
 
-    cout << "Ingrese su clave publica: " << endl; cin >> cla_public;
-    cla_private=Invmod(cla_public, carac);
+    Encriptado(mensaje, clave_pri,mens_encrip);
+    Desencriptado(mensaje, clave_pri, mens_encrip);
 
     system("pause");
     return 0;
